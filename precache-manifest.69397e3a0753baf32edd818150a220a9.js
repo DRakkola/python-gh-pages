09235a234c86e598bd834e434cca3482a05447ab
drakkola <debbichi1997@gmail.com>
2024-01-04 18:32:12 +0100

    Updates

---
+++ precache-manifest.69397e3a0753baf32edd818150a220a9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83eebc5ac7dc2138e2ec5164f67e7ee7",
+    "url": "/python-gh-pages/index.html"
+  },
+  {
+    "revision": "d665f161dfc3c317e015",
+    "url": "/python-gh-pages/static/css/2.61df4d96.chunk.css"
+  },
+  {
+    "revision": "f6088f0e39155d62cc14",
+    "url": "/python-gh-pages/static/css/main.5ae8871b.chunk.css"
+  },
+  {
+    "revision": "d665f161dfc3c317e015",
+    "url": "/python-gh-pages/static/js/2.7391b8d4.chunk.js"
+  },
+  {
+    "revision": "a4357aa61ee627ab4744",
+    "url": "/python-gh-pages/static/js/3.7c6adf38.chunk.js"
+  },
+  {
+    "revision": "d93751a1908fff0fe0b6",
+    "url": "/python-gh-pages/static/js/4.a3416b6e.chunk.js"
+  },
+  {
+    "revision": "f6088f0e39155d62cc14",
+    "url": "/python-gh-pages/static/js/main.535b9479.chunk.js"
+  },
+  {
+    "revision": "98f97f9871158db34987",
+    "url": "/python-gh-pages/static/js/runtime~main.04a3c77a.js"
+  },
+  {
+    "revision": "a9c23c60dddd1d89aaf1885e0ac965a7",
+    "url": "/python-gh-pages/static/media/Soumyajit_Behera-BIT_MESRA.a9c23c60.pdf"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/python-gh-pages/static/media/about.aee0f771.png"
+  },
+  {
+    "revision": "3c9241c2d284b85346b0c8e719568989",
+    "url": "/python-gh-pages/static/media/avatar.3c9241c2.svg"
+  },
+  {
+    "revision": "69948a28ccbd01fdd576340358088e37",
+    "url": "/python-gh-pages/static/media/blog.fb8ab7f2.png"
+  },
+  {
+    "revision": "44190220743b70a49855d73399eec8d8",
+    "url": "/python-gh-pages/static/media/chatify.7206044e.png"
+  },
+  {
+    "revision": "408d49ba10562bd34236c63d4e6af221",
+    "url": "/python-gh-pages/static/media/codeEditor.1b585492.png"
+  },
+  {
+    "revision": "750c7d884d31442e823bce13781275b9",
+    "url": "/python-gh-pages/static/media/emotion.eabfc16e.png"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/python-gh-pages/static/media/home-bg.dc52d5d4.jpg"
+  },
+  {
+    "revision": "1aed08b3e1b66991d921d25dbdcf28ed",
+    "url": "/python-gh-pages/static/media/home-main.1aed08b3.svg"
+  },
+  {
+    "revision": "c512d9da504be4d96313504211775d4e",
+    "url": "/python-gh-pages/static/media/leaf.ff2616f6.png"
+  },
+  {
+    "revision": "cf8c6935f68fde1543684d4a817f213a",
+    "url": "/python-gh-pages/static/media/pre.cf8c6935.svg"
+  },
+  {
+    "revision": "22cb85719800191ea12a9361fbfef9b2",
+    "url": "/python-gh-pages/static/media/suicide.9c86cd71.png"
+  }
+]);
